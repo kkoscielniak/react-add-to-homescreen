4e45061ec8d86eb6be853102c0af3a0a825bb649
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2018-12-03 16:18:48 +0100

    Add conditional rendering for the component

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,31 @@
-/* global describe it expect */
+/* global describe it */
 import React from 'react';
+import assert from 'assert';
 import { shallow } from 'enzyme';
 
-import AddToHomescreen from '../index.js';
+import AddToHomescreen from '../index';
 
 describe('AddToHomescreen Component', () => {
-  it('should render', done => {
+  it('should render on iOS', done => {
+    navigator.__defineGetter__('userAgent', () => 'iPhone');
+
     const wrapper = shallow(<AddToHomescreen />);
-    expect(wrapper).toMatchSnapshot();
+    assert.strictEqual(
+      wrapper.exists('.add-to-home-banner'),
+      true
+    );
+
+    done();
+  });
+
+  it('should not render on another platform', done => {
+    navigator.__defineGetter__('userAgent', () => 'android');
+
+    const wrapper = shallow(<AddToHomescreen />);
+    assert.strictEqual(
+      wrapper.exists('.add-to-home-banner'),
+      false
+    );
 
     done();
   });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import { isIos } from './utils';
+
 class AddToHomescreen extends React.Component {
   render () {
+    const isRunningOnIos = isIos();
+
     return (
-      <div>This is so!</div>
+      <div>
+        {isRunningOnIos ? (
+          <div className='add-to-home-banner'>
+            OMG, this is so iOS!
+          </div>
+        ) : null}
+      </div>
     );
   }
 }

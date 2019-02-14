1722b17a922444f54bc2a4139814dc2764e72bc1
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2019-02-14 11:42:23 +0100

    Add tests for the Add to homescreen button callback

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,4 +1,4 @@
-/* global describe it */
+/* global describe it expect jest */
 import React from 'react';
 import assert from 'assert';
 import { shallow } from 'enzyme';
@@ -26,6 +26,21 @@ describe('AddToHomescreen Component', () => {
       wrapper.exists('.add-to-home-banner'),
       false
     );
+
+    done();
+  });
+
+  it('should call a function on Add To Homescreen button click', done => {
+    navigator.__defineGetter__('userAgent', () => 'iPhone');
+
+    const mockFunction = jest.fn();
+
+    const wrapper = shallow(
+      <AddToHomescreen onAddToHomescreenClick={mockFunction} />
+    );
+
+    wrapper.find('.add-to-home-text a').simulate('click');
+    expect(mockFunction).toHaveBeenCalledTimes(1);
 
     done();
   });
